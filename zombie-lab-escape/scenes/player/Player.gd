f2eb6041bcd46f40582bd2b5b36e3cac1f0dea09
Morgan Bourne <goombaplayz@gmail.com>
2025-10-14 08:24:48 -0700

    trying to get dash function to work

--- zombie-lab-escape/scenes/player/Player.gd
+++ zombie-lab-escape/scenes/player/Player.gd
@@ -7,20 +7,50 @@ var enemy
 var damaged = false
 var direction 
 @export var player_health = 200
-@export var speed = 250
-@export var dash_multiplier =600
+@export var speed = 200
+@export var dash_speed = 250
+@export var dash_duration = 0.2  # Duration of the dash in seconds
+var can_dash = true
+var move = Vector2()
+var dash_timer = 0.0
+var dash_multiplier = 1.7
+var is_dashing
 
-func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
 
-func _physics_process(_delta):
-	if damaged == true:
-		player_hit()
+func _physics_process(delta):
 
+	if Input.is_action_pressed("right"):
+		move.x += 1
+	if Input.is_action_pressed("left"):
+		move.x -= 1
+	if Input.is_action_pressed("down"):
+		move.y += 1
+	if Input.is_action_pressed("up"):
+		move.y -= 1
+	move = Vector2.ZERO
+
+	# Handle dash function
+	if Input.is_action_just_pressed("dash") and can_dash:
+		is_dashing = true
+		dash_timer = dash_duration
+		can_dash = false
+		$dash_cooldown.start()
+	
+	if is_dashing:
+		velocity = move.normalized() * dash_multiplier  * dash_speed 
+		dash_timer -= delta
+		if dash_timer <= 0:
+			is_dashing = false
 	else:
-		get_input()
+		velocity = move.normalized() * speed
+	# Move the player
 	move_and_slide()
+
+
+
+
+func _on_dash_cooldown_timeout() -> void:
+	can_dash = true
 
 func _on_damage_area_area_entered(area: Area2D) -> void:
 	if area.is_in_group('enemy_hitbox'):
@@ -32,7 +62,7 @@ func _on_damage_area_area_entered(area: Area2D) -> void:
 func player_hit():
 	player_health -= 20
 	direction = (enemy.global_position + self.global_position).normalized()
-	velocity = (direction * speed )* 2
+	velocity = (direction * speed )* 300
 	print(player_health)
 	damaged = false 
 
