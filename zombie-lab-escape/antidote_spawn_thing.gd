457de755b250cf43d4109be4acfdcf95a084b062
morgan <goombaplayz@gmail.com>
2025-10-17 01:16:42 -0700

    started things off

---
+++ zombie-lab-escape/antidote_spawn_thing.gd
@@ -0,0 +1,20 @@
+extends Node2D
+var player_safe = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		pass
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
